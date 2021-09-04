6f34a2de44f9e5e2e95969b2263da46008cd6eed
Виктор Гладков <vic.gladkov@gmail.com>
2021-09-05 04:21:58 +0600

    Optimization fixes.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,24 +3,27 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
         for (int i = 0; i < storage.length; i++) {
             if (storage[i] == null) {
                 storage[i] = r;
+                size++;
                 break;
             }
         }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
@@ -29,16 +32,11 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        Resume tempResume;
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                if (i == size() - 1) {
-                    storage[i] = null;
-                } else {
-                    tempResume = storage[i + 1];
-                    storage[i + 1] = storage[i];
-                    storage[i] = tempResume;
-                }
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
             }
         }
     }
@@ -47,19 +45,12 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] allResumes = new Resume[size()];
+        Resume[] allResumes = new Resume[size];
         if (allResumes.length >= 0) System.arraycopy(storage, 0, allResumes, 0, allResumes.length);
         return allResumes;
     }
 
     int size() {
-        int count = 0;
-        for (Resume resume : storage) {
-            if (resume == null) {
-                break;
-            }
-            count++;
-        }
-        return count;
+        return size;
     }
 }
