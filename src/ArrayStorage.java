a3f245585c0a61a9d674ac49c8cc2a33dff87350
Виктор Гладков <vic.gladkov@gmail.com>
2021-09-06 00:28:47 +0600

    save method changed. break in delete added.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -13,13 +13,8 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                size++;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
@@ -37,6 +32,7 @@ public class ArrayStorage {
                 storage[i] = storage[size - 1];
                 storage[size - 1] = null;
                 size--;
+                break;
             }
         }
     }
